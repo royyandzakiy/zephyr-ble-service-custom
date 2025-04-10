0651911a50ea73f3375ad5ea4e53f17c1e58e242
royyandzakiy <royyan.abdul@gmail.com>
2025-04-11 00:05:28 +0700

    added high throughput settings, should test out

--- src/ble/app_ble.c
+++ src/ble/app_ble.c
@@ -15,10 +15,70 @@ static const struct bt_data ad[] = {
     BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
 };
 
+// static struct bt_conn *current_conn;
+
+// static void connected(struct bt_conn *conn, uint8_t err)
+// {
+//     if (err) {
+//         LOG_ERR("Failed to connect (err %u)", err);
+//         return;
+//     }
+
+//     LOG_INF("Connected");
+//     current_conn = bt_conn_ref(conn);
+
+//     struct bt_le_conn_param param = BT_LE_CONN_PARAM_DEFAULT;
+//     struct bt_conn_le_phy_pref phy_pref = {
+//         .tx_phys = BT_GAP_LE_PHY_2M,
+//         .rx_phys = BT_GAP_LE_PHY_2M,
+//     };
+
+//     int err_conn_param = bt_conn_le_param_update(current_conn, &param);
+//     if (err_conn_param) {
+//         LOG_WRN("Failed to update connection parameters (err %d)", err_conn_param);
+//     } else {
+//         LOG_INF("Connection parameters update requested.");
+//     }
+
+//     int err_phy = bt_conn_le_phy_update(current_conn, &phy_pref);
+//     if (err_phy) {
+//         LOG_WRN("Failed to update PHY to 2M (err %d)", err_phy);
+//     } else {
+//         LOG_INF("PHY update to 2M requested.");
+//     }
+
+//     int err_dle = bt_conn_le_data_len_update(current_conn, BT_LE_DATA_LEN_MAX);
+//     if (err_dle) {
+//         LOG_WRN("Failed to update data length extension (err %d)", err_dle);
+//     } else {
+//         LOG_INF("Data length extension update requested.");
+//     }
+// }
+
+// static void disconnected(struct bt_conn *conn, uint8_t reason)
+// {
+//     LOG_INF("Disconnected (reason %u)", reason);
+//     if (current_conn) {
+//         bt_conn_unref(current_conn);
+//         current_conn = NULL;
+//     }
+// }
+
+// struct bt_conn_cb connection_callbacks = {
+//     .connected = connected,
+//     .disconnected = disconnected,
+// };
+
 int app_ble_init(void)
 {
 
     int err = 0;
+
+    // err = bt_conn_cb_register(&connection_callbacks);
+    // if (err) {
+    //     LOG_ERR("Failed to register connection callbacks (err %d)", err);
+    //     return err;
+    // }
 
     /* Enable BLE peripheral */
     err = bt_enable(NULL); // NULL is for bluetooth ready callback
